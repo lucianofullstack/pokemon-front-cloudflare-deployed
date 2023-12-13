84d9488cabfd73c9b152accc0f8c2f658877a261
Luciano Pereira <127886247+lucianofullstack@users.noreply.github.com>
2023-12-13 01:34:51 +0100

    deploy

--- src/redux/actions.jsx
+++ src/redux/actions.jsx
@@ -1,6 +1,6 @@
 import
 axios from 'axios'
-axios.defaults.baseURL =  'https://pokemon-back.adaptable.app/pokemons'
+axios.defaults.baseURL =  'https://pokemon-back.adaptable.app'
 
 export const 
 
